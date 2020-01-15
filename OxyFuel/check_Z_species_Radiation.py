0bc747631715acca7b69b152ee4897e0e61fd224
Maximilian Hansinger <maximilian.hansinger@unibw.de>
2020-01-15 19:14:37 +0100

    Radiation

---
+++ OxyFuel/check_Z_species_Radiation.py
@@ -0,0 +1,206 @@
+'''
+This file is to check if the T shift is also present if Z is computed with species (and not f_Bilger)
+'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from os.path import join
+import cantera as ct
+
+######################################
+# some parameters
+Z_st = 0.0535
+Y_CH4_F = 0.8668
+Y_H2_F = 0.1331
+Y_O2_Ox = 0.2549
+
+# Oxygen-Fuel to mass ratio. See Peters, p.172
+nu = (1-Z_st)/Z_st * Y_O2_Ox/(Y_CH4_F+Y_H2_F)
+
+try:
+    from matplotlib2tikz import save as tikz_save
+except:
+    print('Install matplotlib2tikz: pip3 install matplotlib2tikz')
+
+
+location_dict =['01','03','05','10','15','20','30']
+
+mypath = 'scatter' #input('Where did you save the scatter data?, type in: ')
+
+files = os.listdir(mypath)
+
+LESdata = {}
+
+# read in the files
+for file in files:
+     if file.endswith(".txt") and file.startswith("scatter_xD"):
+         print('Reading in data from: '+file)
+         df = pd.read_csv(mypath+'/'+file,sep='\t')
+         # generate file name
+         name = file.split('.txt')[0]
+         LESdata[name] = df
+
+scatterPlanes = list(LESdata.keys())
+# get the order of the files
+file_order = [int(f.split('xD')[1]) for f in scatterPlanes]
+#sort_vec = sorted(range(len(file_order)), key=lambda k: file_order[k])
+scatterPlanes = [x for _,x in sorted(zip(file_order,scatterPlanes))]
+
+############################################
+# plots
+def plotZ_compare(sampleSize=50000):
+    # creates a scatter plot of the defined species over the mixture fraction
+
+    # loop over the different planes:
+    try:
+        for i in range(len(scatterPlanes)):
+            plt.figure(i + 1)
+            thisData = LESdata[scatterPlanes[i]].sample(sampleSize)
+            thisData['Z_species'] = (nu* (thisData['CH4'] + thisData['H2']) - thisData['O2'] + Y_O2_Ox)/(nu*(Y_H2_F + Y_CH4_F) + Y_O2_Ox)
+            plt.scatter(thisData['f_Bilger'], thisData['T'], marker='.', s=0.2,c='k')
+            plt.scatter(thisData['Z_species'], thisData['T'], marker='.', s=0.2, c='r')
+            plt.xlabel('Mixture fraction')
+            plt.ylabel('T')
+            plt.xlim(0, 0.3)
+            plt.plot([Z_st,Z_st],[0,2500],'--',lw=0.5)
+            minSpec = min(thisData['T']) * 0.999
+            maxSpec = max(thisData['T']) * 1.05
+            plt.ylim(minSpec, maxSpec)
+            plt.legend(['Bilger','Species','Z_st'])
+            plt.title('Scatter plot at: x/D=' + str(int(location_dict[i]) ))
+
+    except:
+        print('Species not available')
+
+    plt.show(block=False)
+
+
+#############################################
+# RADIATION
+#############################################
+'''
+Estimate the radiation of the Flame based on Optically-Thin Layer
+check TNF Radiation models and TNF Proceeding Nr6
+
+Under the assumptions that these flames are optically thin, such that each radiating point source has an 
+unimpeded isotropic view of the cold surroundings, the radiative loss rate per unit volume may be calculated as:
+
+Q(T,species) = 4  SUM{pi*ap,i} *(T4-Tb4)
+
+where
+
+    sigma=5.669e-08 W/m2K4 is the Steffan-Boltzmann constant, 
+    SUM{ } represents a summation over the species included in the radiation calculation, 
+    pi is the partial pressure of species i in atmospheres (mole fraction times local pressure),
+    ap,i is the Planck mean absorption coefficient of species i,
+    T is the local flame temperature (K), and 
+    Tb is the background temperature (300K unless otherwise specified in the experimental data).
+
+Note that the Tb term is not consistent with an emission-only model. 
+It is included here to eliminate the unphysical possibility of calculated temperatures in the coflowing air 
+dropping below the ambient temperature. In practice, this term has a negligible effect on results.
+
+CO2 and H2O are the most important radiating species for hydrocarbon flames. As an example, 
+Jay Gore reports that the peak temperature in a strained laminar flame calculation decreased by 50K when 
+radiation by CO2 and H2O was included. Inclusion of CH4 and CO dropped the peak temperature by another 5K. 
+On the rich side of the same flames the maximum effect of adding the CH4 and CO radiation was an 8K reduction 
+in temperature (from 1280 K to 1272 K for a particular location). These were OPPDIF calculations 
+(modified to include radiation) of methane/air flames with 1.5 cm separation between nozzles and 8 cm/s fuel and air velocities.
+
+ap = c0 + c1*(1000/T) + c2*(1000/T)2 + c3*(1000/T)3 + c4*(1000/T)4 + c5*(1000/T)5
+
+The coefficients are:
+ 	 H2O	        CO2
+c0	-0.23093	    18.741
+c1	-1.12390	    -121.310
+c2	9.41530	        273.500
+c3	-2.99880	    -194.050
+c4	0.51382	        56.310
+c5	-1.86840E-05	-5.8169
+
+A fourth-order polynomial in temperature is used for CH4:
+
+ap,ch4 = 6.6334 – 0.0035686*T + 1.6682e-08*T2 + 2.5611e-10*T3 – 2.6558e-14*T4
+
+A fit for CO is given in two temperature ranges:
+
+ap,co = c0+T*(c1 + T*(c2 + T*(c3 + T*c4)))
+'''
+
+# set up cantera gas to get molar weights
+gas = ct.Solution('gri30.xml')
+
+# sampled species
+species = ['CO','H2O','H2','O2','CH4','OH','CO2']
+
+T_b = 294 # Background temperature [K]
+
+sigma=5.669e-08 # W/m2K4 is the Steffan-Boltzmann constant,
+
+# Absorption coefficient for CO2
+ap_CO2 = lambda T: 18.741 -121.310*(1000/T) + 273.500*(1000/T)**2 -194.050*(1000/T)**3 + 56.310*(1000/T)**4 -5.8169*(1000/T)**5
+
+# Absorption coefficient for H2O
+ap_H2O = lambda T: 	-0.23093 -1.12390*(1000/T) + 9.41530*(1000/T)**2 -2.99880*(1000/T)**3 + 0.51382*(1000/T)**4  -1.86840E-05*(1000/T)**5
+
+# Absorption coefficient for CH4
+ap_CH4 = lambda T: 6.6334- 0.0035686 * T + 1.6682e-08 * T**2 + 2.5611e-10 * T**3 - 2.6558e-14 * T**4
+
+# Absorption coefficient for CO
+ap_CO = lambda T: 10.09 - 0.001183 * T + 4.775e-06 * T**2  - 5.872e-10 * T**3 - 2.533e-14 * T**4
+
+
+# plots
+def plot_Radiation(sampleSize=50000):
+    # creates a scatter plot of the defined species over the mixture fraction
+
+    for i in range(len(scatterPlanes)):
+        plt.figure(i + 1)
+        thisData = LESdata[scatterPlanes[i]].sample(sampleSize)
+        thisData_mole = compute_mol_fractions(thisData)
+        T = thisData_mole['T']
+
+        # absorption coefficients x molar fraction of species
+        Q_CO2 = thisData_mole['x_CO2'] * ap_CO2(T)
+        Q_H2O = thisData_mole['x_H2O'] * ap_H2O(T)
+        Q_CH4 = thisData_mole['x_CH4'] * ap_CH4(T)
+        Q_CO = thisData_mole['x_CO'] * ap_CO(T)
+
+        thisData_mole['Q_radiation'] = 4*sigma*(T**4- T_b**4)*(Q_CO2 + Q_H2O + Q_CH4 + Q_CO)
+
+        plt.scatter(thisData_mole['f_Bilger'],thisData_mole['Q_radiation'],s=0.2)
+
+        plt.xlabel('Mixture fraction')
+        plt.ylabel('Q_radiation [W]')
+        plt.xlim(0, 0.3)
+        #plt.plot([Z_st,Z_st],[0,2500],'--',lw=0.5)
+        minSpec = min(thisData_mole['Q_radiation']) * 0.999
+        maxSpec = max(thisData_mole['Q_radiation']) * 1.05
+        plt.ylim(minSpec, maxSpec)
+        #plt.legend(['Bilger','Species','Z_st'])
+        plt.title('Scatter plot at: x/D=' + str(int(location_dict[i]) ))
+
+    plt.show(block=False)
+
+
+def compute_mol_fractions(thisData):
+
+    assert type(thisData) == pd.DataFrame
+
+    Nenner = [(thisData[f] / gas.molecular_weights[gas.species_index(f)]) for f in species]
+    Nenner = sum(Nenner)
+
+    thisData['x_CO'] = thisData['CO'] / gas.molecular_weights[gas.species_index('CO')] / Nenner
+    thisData['x_CO2'] = thisData['CO2'] / gas.molecular_weights[gas.species_index('CO2')] / Nenner
+    thisData['x_H2O'] = thisData['H2O'] / gas.molecular_weights[gas.species_index('H2O')] / Nenner
+    thisData['x_CH4'] = thisData['CH4'] / gas.molecular_weights[gas.species_index('CH4')] / Nenner
+    thisData['x_H2'] = thisData['CO'] / gas.molecular_weights[gas.species_index('H2')] / Nenner
+    thisData['x_OH'] = thisData['OH'] / gas.molecular_weights[gas.species_index('OH')] / Nenner
+    thisData['x_O2'] = thisData['O2'] / gas.molecular_weights[gas.species_index('O2')] / Nenner
+
+    return thisData
+
+
+
---
+++ TNF14_data/radial_samples_TRMS.py
@@ -0,0 +1,104 @@
+'''
+This is to postprocess the data again in the desired format as required for the TNF14 workshop
+
+@author: mhansinger
+
+last modified: 12.2.19
+
+'''
+
+#!/usr/bin/python
+
+'''
+This file extracts radial field values from OpenFoam data
+
+What you have to do is:
+- check if scalarTail and speciesTail in line 18&19 corresponds to your sampled output fiĺes in sampleDict/TIME
+- datapoints and noFiles ind line 22 & 23 is correct, therefore you have to check the length of your files!
+- check if Output_np and Output_df in line 120 and 129 comprise all the arrays you need or if something is missing!
+
+'''
+
+import numpy as np
+import pandas as pd
+from os import listdir
+from os.path import isdir
+
+
+def radial_samples_reacting(case_path):
+    # this one has to be correct and is different for inert and reacting
+    try:
+        scalarTail = '_T_RMSMean.xy'
+
+    except:
+
+        scalarTail = '_f_BilgerMean_f_BilgerPrime2Mean_TMean_TPrime2Mean_CH4Mean_' \
+                 'CH4Prime2Mean_H2OMean_H2OPrime2Mean_CO2Mean_CO2Prime2Mean_O2Mean_O2Prime2Mean_COMean_COPrime2Mean_H2Mean_H2Prime2Mean.xy'
+
+        print('NO DIFFUSIVE FLUXES ARE PRESENT!')
+
+    print(scalarTail+'\n')
+
+    # initialize arrays
+    datapoints = 200
+    noFiles = 40
+
+    # represents the different locations you defined in the sample dict file
+    nLocation = [0, 1, 2, 3, 4, 5, 6]
+    location_dict = ['010', '050', '100', '120', '150', '200', '300']
+
+    # get all time steps
+    times = listdir(case_path+'/postProcessing/sampleDict_RMS/')
+    # remove the .txt files in the times list as they are also stored in the sampleDict folder
+    times = [f for f in times if f[-3:] != 'txt']
+
+    # loop over the time steps for averaging
+
+    for n in range(0, len(nLocation)):
+        arrayTRMS = np.zeros((datapoints))
+
+
+        for time in times:
+            for j in range(0, noFiles):
+                try:
+                    dataScalar = np.loadtxt(case_path+'/postProcessing/sampleDict_RMS/' + time + '/line_x' +
+                                            str(nLocation[n]) + '-r' + str(j) + scalarTail)
+                except:
+                    print('Check the file name and/or path of scalar fields! Something is wrong')
+                    print(case_path + '/postProcessing/sampleDict_RMS/' + time + '/line_x' + str(nLocation[n]) + '-r' + str(j) + scalarTail)
+
+                # there is the position of the T column in your data set;
+                # check for consistency! they are summed up for different j
+                arrayTRMS += dataScalar[:, 3]
+
+
+                # compute the radius only once at the beginning
+                if j == 0 and time == times[0]:
+                    arrayYPos = dataScalar[:, 1]    # m
+                    arrayZPos = dataScalar[:, 2]    # m
+                    arrayDist = np.sqrt(arrayYPos * arrayYPos + arrayZPos * arrayZPos)
+
+        # set up to write the output file!
+        Output_np = np.array((arrayDist, arrayTRMS))
+
+        # Divide by the number of files and times and transpose
+        Output_T = Output_np.T
+        Output_T = Output_T / (noFiles * len(times))
+        # set up dataframe to write as CSV
+        Output_df = pd.DataFrame(Output_T)
+
+        # Name correctly the output columns
+        Output_df.columns = ['r_in_m', 'T_sgs_RMS']
+
+        Output_df['r_in_m'] = arrayDist
+
+        # remove the nan
+        Output_df = Output_df.fillna(0)
+
+        # write one output file for each position
+        output_name = case_path+'/postProcessing/sampleDict_RMS/' + 'line_xD' + location_dict[n] + '_T_sgs_RMS.txt'
+        pd.DataFrame.to_csv(Output_df, output_name, index=False, sep='\t')
+
+
+if __name__ == "__main__":
+    radial_samples_reacting('.')
