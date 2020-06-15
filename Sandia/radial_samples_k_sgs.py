0be0b96852bef7db13634bae35dbb3dcd065dee4
Maximilian Hansinger <maximilian.hansinger@unibw.de>
2020-06-15 22:45:11 +0200

    TKE Sandia

--- Sandia/radial_samples_k_sgs.py
+++ Sandia/radial_samples_k_sgs.py
@@ -3,19 +3,14 @@ This is to postprocess the data again in the desired format as required for the 
 
 @author: mhansinger
 
-last modified: 12.2.19
-
+last change: 8.1.2018
 '''
 
 #!/usr/bin/python
-
 '''
+
 This file extracts radial field values from OpenFoam data
 
-What you have to do is:
-- check if scalarTail and speciesTail in line 18&19 corresponds to your sampled output fiĺes in sampleDict/TIME
-- datapoints and noFiles ind line 22 & 23 is correct, therefore you have to check the length of your files!
-- check if Output_np and Output_df in line 120 and 129 comprise all the arrays you need or if something is missing!
 
 '''
 
@@ -27,18 +22,25 @@ from os.path import isdir
 
 def radial_samples_reacting(case_path):
     # this one has to be correct and is different for inert and reacting
+    #scalarTail = '_f_BilgerMean_f_BilgerPrime2Mean_TMean_TPrime2Mean_CH4Mean_CH4Prime2Mean_H2OMean_H2OPrime2Mean_' \
+    #             'CO2Mean_CO2Prime2Mean_O2Mean_O2Prime2Mean_COMean_COPrime2Mean_H2Mean_H2Prime2Mean.xy'
 
-    scalarTail = '_k_sgsMean.xy'
+    scalarTail = '_k_sgs.xy'
 
-    print(scalarTail+'\n')
 
     # initialize arrays
     datapoints = 200
     noFiles = 40
 
+    # Diameter
+    d_Sandia = 0.0072
+
+    # f_BilgerMax Sandia
+    f_max = 1
+
     # represents the different locations you defined in the sample dict file
-    nLocation = [0, 1, 2, 3, 4, 5, 6]
-    location_dict = ['010', '050', '100', '120', '150', '200', '300']
+    nLocation = [0, 1, 2, 3, 4]
+    location_dict = [ '07.5',  '15',  '30', '45', '60']
 
     # get all time steps
     times = listdir(case_path+'/postProcessing/sampleDict_k_sgs/')
@@ -56,7 +58,6 @@ def radial_samples_reacting(case_path):
         arrayWRMS = np.zeros((datapoints))
         arrayk_sgs = np.zeros((datapoints))
 
-
         for time in times:
             for j in range(0, noFiles):
                 try:
@@ -65,6 +66,7 @@ def radial_samples_reacting(case_path):
                 except:
                     print('Check the file name and/or path of scalar fields! Something is wrong')
                     print(case_path + '/postProcessing/sampleDict_k_sgs/' + time + '/line_x' + str(nLocation[n]) + '-r' + str(j) + scalarTail)
+                    print(' ')
 
                 # there is the position of the T column in your data set;
                 # check for consistency! they are summed up for different j
@@ -85,6 +87,8 @@ def radial_samples_reacting(case_path):
                     arrayZPos = dataScalar[:, 2]    # m
                     arrayDist = np.sqrt(arrayYPos * arrayYPos + arrayZPos * arrayZPos)
 
+            # loop 2 end
+
 
         # set up to write the output file!
         Output_np = np.array((arrayDist, arrayk_sgs,arrayURMS,arrayVRMS,arrayWRMS))
@@ -100,9 +104,14 @@ def radial_samples_reacting(case_path):
 
         Output_df['r_in_m'] = arrayDist
 
+        Output_df['r_in_mm'] = arrayDist * 1000
+
+        Output_df['r_over_d'] = Output_df['r_in_m'].values / 0.0072
+
         Output_df['TKE_resolved'] = (Output_df.U_Prime.values +Output_df.V_Prime.values + Output_df.W_Prime.values)/ 2 # np.sqrt(Output_df.U_Prime.values ** 2 +Output_df.V_Prime.values** 2 + Output_df.W_Prime.values** 2)/2
 
         Output_df['TKE_ratio'] = Output_df.TKE_sgs.values / (Output_df.TKE_sgs.values + Output_df.TKE_resolved.values)
+
 
         # remove the nan
         Output_df = Output_df.fillna(0)
@@ -112,5 +121,5 @@ def radial_samples_reacting(case_path):
         pd.DataFrame.to_csv(Output_df, output_name, index=False, sep='\t')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     radial_samples_reacting('.')
